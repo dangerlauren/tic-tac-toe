413149ddaf9dfbeaa34c4be19452673baca4edc1
Lauren Grant <dangerlauren@gmail.com>
2015-09-09 15:20:12 -0500

    added player buttons

--- tic-tac-toe/tic_tac_toe.js
+++ tic-tac-toe/tic_tac_toe.js
@@ -6,24 +6,91 @@ $(document).ready(function () {
 // define variables for player 1, player 2, winning combinations
 var firstPlayer
 var secondPlayer
+var emptySpace
+var turn = 0
+// var whoWon = function(){
+// 	var playerX = ;
+// 	var playerO = ;
+// 	var catsGame = ;
 
-// create buttons with listeners
-$("button").on("click", function (){
-	alert($(this).text);
+// upon refresh, ask player 1 to choose X or O
+alert("Player 1 select X or O");
+
+// create buttons with listeners for the players
+
+// if player 1 chooses X, else chooses O
+$("button.player").on("click", function (){
+	if ($(this).hasClass("playerX")) {
+		firstPlayer = "X"
+		turn = 1
+		alert("You chose X!");
+	}
+
+	else {
+	firstPlayer = "O"
+	turn = 1
+	alert("You chose O!")
+	}
+	
 });
 
-// form for players 1 and 2 to enter their names (or prompts?)
-// define variables for the winning combinations - use arrays
-// if / else if / else statements for the patterns that will win with X
-// check rows, check columns, check diagonals using && ==
-// use an alert for the winner
-// if / else if / else statements for the patterns that will win with O
-// check rows, check columns, check diagonals using && ==
-// use an alert for the winner
-// 
-// function for draw
-// function for reset
+// create buttons with listeners for the gameboard
+// all buttons should be blank, want the button text to change to either X or O upon click
+$("button.squares").on("click", function (){
+	($(this).html("EMPTY"));
+	// if current turn is player1, clicked button should return X
+var gamePlay = (function(){
+	if (turn % 2 === 0) {
+		player2
+	}
 
+});
+
+
+	// if current turn is player2, clicked button should return O
+	// if player1 or player2 clicks a square that is already chosen
+	// alert("Please choose an empty space")
+	
+});
+
+// define variables for the winning combinations
+// if the following square combinations: 
+// [1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7] 
+// have all X or all O, then alert appropriate winner
+
+// define variable for tie game
+// if no winning combinations, then alert cat's draw
+
+// create a function to reset and start a new game
+
+
+// #########  MISC  ########### 
+
+// $("button.player").on("click", function (){
+// 	if ($(this).hasClass("playerO")) {
+// 	alert("You chose O!");
+// 	};
+// });
+
+// depending upon choice of player 1, first box clicked will be X or O
+// $("button.playerX").on("click", function(){
+// 	firstPlayer = "X"
+// 	turn = 1
+// });
+
+// $("button.playerO").on("click", function(){
+// 	firstPlayer = "O"
+// 	turn = 2
+// });
+
+// var game = (function () {
+// 	var playerX = ();
+// 	var playerO = ();
+
+// var findWinner = function(){
+// 	var waysToWin = [ [1 ,2 ,3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]],
+// }
+// });
 
 
 // call playGame function
