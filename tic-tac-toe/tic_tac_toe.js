cdac2edf1eeb13505aeb74f9bcf8dff93a4441cb
Lauren Grant <dangerlauren@gmail.com>
2015-09-09 10:31:19 -0500

    created html, js, readme files to begin game

---
+++ tic-tac-toe/tic_tac_toe.js
@@ -0,0 +1,15 @@
+// define variables for player 1 and player 2
+	// player 1 is always X, player 2 is always O
+// form for players 1 and 2 to enter their names (or prompts?)
+// define variables for the winning combinations - use arrays
+// if / else if / else statements for the patterns that will win with X
+// check rows, check columns, check diagonals using && ==
+// use an alert for the winner
+// if / else if / else statements for the patterns that will win with O
+// check rows, check columns, check diagonals using && ==
+// use an alert for the winner
+// 
+// function for draw
+// function for reset
+// 
+
