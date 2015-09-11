f4e467c30e5785442b40a87f6f9fbf427042fe20
Lauren Grant <dangerlauren@gmail.com>
2015-09-10 21:54:46 -0500

    moved images file and alternate js file out of main folder

---
+++ button.js
@@ -0,0 +1,13 @@
+// $("button").on("click", function (){
+// 	alert("hey");
+// });
+
+// $("button").on("click", function (){
+// 	alert( $(this).text() );
+// });
+
+$("button").on("click", function (){
+	if ($(this).hasClass("x-square")) {
+	alert("You chose X");
+	}
+});
